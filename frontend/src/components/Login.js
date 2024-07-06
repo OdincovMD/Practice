a4471719533c310c15d4d1f08ee8e40a81cee52e
Kegelik Nikolay <cheshire.with.milk@gmail.com>
2024-07-06 23:07:59 +0300

    Commit 7: separated directory for frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,15 +1,12 @@
 import React from "react"
 
-class Register extends React.Component {
+class Login extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            lastName: "",
-            firstName: "",
             login: "",
-            password: "",
-            rep_password: "",
+            password: ""
         }
     }
 
@@ -17,15 +14,7 @@ class Register extends React.Component {
         return (
             <form ref={(el) => this.myForm = el}>
                 <input
-                    className="top"
-                    placeholder="Фамилия"
-                    onChange={(ans) => { this.setState({ lastName: ans.target.value }) }}
-                />
-                <input
-                    placeholder="Имя"
-                    onChange={(ans) => { this.setState({ firstName: ans.target.value }) }}
-                />
-                <input
+                    className = "top"
                     placeholder="Логин"
                     onChange={(ans) => { this.setState({ login: ans.target.value }) }}
                 />
@@ -34,24 +23,19 @@ class Register extends React.Component {
                     placeholder="Пароль"
                     onChange={(ans) => { this.setState({ password: ans.target.value }) }}
                 />
-                <input
-                    type="password"
-                    placeholder="Подтвердите пароль"
-                    onChange={(ans) => { this.setState({ password: ans.target.value }) }}
-                />
                 <br />
                 <button
                     type="button"
                     onClick={() => {
                         this.myForm.reset()
-                        this.onRegister(this.state)
+                        this.onLogin(this.state)
                     }
                     }>
-                    Регистрация
+                    Войти
                 </button>
                 <br /><br />
                 <p>
-                    Уже зарегестрированы?&nbsp;
+                    Ещё не зарегестрированы?&nbsp;
                 </p>
                 <button
                     className="link"
@@ -59,15 +43,16 @@ class Register extends React.Component {
                     onClick={() => {
                         this.myForm.reset()
                         this.setState({ login: "", password: "" })
-                        this.props.onChange("login")
+                        this.props.onChange("register")
                     }
                     }>
-                    Войти
+                    Регистрация
                 </button>
             </form>
         )
     }
 
+    // answer = XMLHttpRequest()
 }
 
-export default Register
+export default Login
